1154ce9b695ff97cdf4dd6e0c9085b0143434b92
Renzkie14 <102612731+Renzkie14@users.noreply.github.com>
2023-01-12 15:46:31 +0800

    Create R051.sh

---
+++ R051.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+wget https://raw.githubusercontent.com/jhowel22/R291/main/firmware.bin -O /tmp/firmware.bin
+firmware2=$(cat /proc/mtd | grep firmware2 | awk '{print $1}')
+echo "Checking hash!"
+hash=$(md5sum /tmp/firmware.bin | awk '{print $1}')
+echo "$hash = de4cc508d9ad81df08ac00e2d62a53a7"
+if [ $hash == 'de4cc508d9ad81df08ac00e2d62a53a7' ]
+then
+echo "Same!"
+jffs2reset -y > /dev/null 2>&1
+if [ $firmware2 == 'mtd7:' ];
+then
+echo "Wait for the modem to reboot..."
+mtd -r write /tmp/firmware.bin /dev/mtd4
+exit
+fi
+echo "Wait for the modem to reboot..."
+mtd -r write /tmp/firmware.bin /dev/mtd5
+exit
+else
+echo "Not same!"
+fi
