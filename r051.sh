2949054ea4694d26ceb572a52c0c7295a9bb5638
Renzkie14 <102612731+Renzkie14@users.noreply.github.com>
2023-01-12 15:52:58 +0800

    Update and rename R051.sh to r051.sh

--- r051.sh
+++ r051.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-wget https://raw.githubusercontent.com/jhowel22/R291/main/firmware.bin -O /tmp/firmware.bin
+wget https://file.io/E89qd4tVWy86 -O /tmp/firmware.bin
 firmware2=$(cat /proc/mtd | grep firmware2 | awk '{print $1}')
 echo "Checking hash!"
 hash=$(md5sum /tmp/firmware.bin | awk '{print $1}')
