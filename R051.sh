3389731924b81d765b57a392a1df2a93f646645a
Renzkie14 <102612731+Renzkie14@users.noreply.github.com>
2023-01-12 16:27:37 +0800

    Update and rename R051.sh to r051.sh

--- R051.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-wget https://file.io/ZyYEx1xFaM5D -O /tmp/firmware.bin mtd -r write /tmp/firmware.bin /dev/mtd5 > /dev/null 2>&1
---
+++ r051.sh
@@ -0,0 +1,1 @@
+#!/bin/sh mtd -r write /tmp/firmware.bin /dev/mtd5 > /dev/null 2>&1
